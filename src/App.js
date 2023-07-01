f7ec15e11f7b953b5ef87a4851ae704eeb67799b
punit-zignallabs <punit.lakhwani@zignallabs.com>
2023-07-01 16:43:48 +0530

    Adding deploy stage to punit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> by Punit sir .       </p>
+          Edit <code>src/App.js</code> by Punit .       </p>
         <a
           className="App-link"
           href="https://reactjs.org"
